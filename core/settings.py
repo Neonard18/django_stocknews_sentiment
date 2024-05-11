5022bcbdd21f564a92b591a5bf4b71e640be87b3
Ngwu Emmanuel Chibuike <ngwuemmanuel84@gmail.com>
2024-05-11 22:15:10 +0100

    modified settings.py

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8-k(r2*j3ryg_2n280q^-!%cl)t-o5g_vdb4^q)5n_a91cges0'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-8-k(r2*j3ryg_2n280q^-!%cl)t-o5g_vdb4^q)5n_a91cges0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True')=='True'
