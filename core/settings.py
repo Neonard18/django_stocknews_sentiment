8d3f9ff780803e99b444d4c789b1004e6eda9351
Ngwu Emmanuel Chibuike <ngwuemmanuel84@gmail.com>
2024-05-11 19:08:30 +0100

    modified settings.py

--- core/settings.py
+++ core/settings.py
@@ -125,8 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-if not DEBUG:
-    STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_ROOT = BASE_DIR/'static'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # STATICFILES_DIRS = [
